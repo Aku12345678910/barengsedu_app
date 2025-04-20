173fb39849b6657b538ddd2a2253ba21e1299c09
Aku12345678910 <fi4020308@gmail.com>
2025-04-21 06:44:54 +0700

    Create Display Slider

--- app/pages/home.js
+++ app/pages/home.js
@@ -6,21 +6,16 @@ import { AuthContext } from '../context/AuthContext'
 import WelcomeHeader from '../components/welcomeHeader'
 import SearchBar from '../components/SearchBar'
 import GlobalApi from '../Shared/GlobalApi'
+import Slider from '../components/Slider'
 
 export default function Home() {
         const {userData,setUserData}=useContext(AuthContext)
-        useEffect(()=>{
-            getSlider();
-        },[])
-
-        const  getSlider=async()=>{
-            const result=(await GlobalApi.getSlider()).data;
-            console.log("Result",result)
-        }
+        
     return (
         <View style={{padding:20}}>
             <WelcomeHeader/>
             <SearchBar/>
+            <Slider/>
         </View>
     )
 }
